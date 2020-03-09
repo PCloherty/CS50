a5185bf2756ab2231f77b5918cf1b35a39011781
PCloherty <PCloherty@users.noreply.github.com>
2020-03-09 17:11:58 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ cash.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{   
+
+    //change as a float
+    float changeOwed = get_float("How much change is owed(with decimal point)? ");
+    //change as an int from the float
+    int change = round(changeOwed * 100);
+
+    //the amount of each coin
+    int quarters =  0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
+    
+
+    if (change > 0)
+    {
+        while (change >= 25) 
+        {
+            change = change - 25;
+            quarters++;
+        }
+        while (change >= 10) 
+        {
+            change = change - 10;
+            dimes++;
+        }
+        while (change >= 5) 
+        {
+            change = change - 5;
+            nickels++;
+        }
+        while (change >= 1) 
+        {
+            change = change - 1;
+            pennies++;
+        }
+        //list and quantity of coins
+        printf("%d quarter(s)\n", quarters);
+        printf("%d dime(s)\n", dimes);
+        printf("%d nickel(s)\n", nickels);
+        printf("%d pennie(s)\n", pennies);
+    } 
+    else 
+    {
+        main();
+    }
+}
