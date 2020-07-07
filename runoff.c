60296de96a4646fad85230964c6bd31d3afe633f
pcloherty <pcloherty@users.noreply.github.com>
2020-07-07 17:11:00 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -143,6 +143,15 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
+    for(int i=0; i<candidate_count; i++)
+    {
+        if (candidates[i].eliminated==false)
+        {
+            candidates[i].votes++;
+            
+        }
+    }
+    
     // TODO
     return;
 }
