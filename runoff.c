c41e5aeebfca384a0a52a61ec7c281020a5b0a91
pcloherty <pcloherty@users.noreply.github.com>
2020-07-08 14:59:40 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -181,8 +181,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    int min = voter_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if( candidates[i].eliminated == false && candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }    
     // TODO
-    return 0;
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
