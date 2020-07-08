272b88cc934ad09305a805cea9a639133add764e
pcloherty <pcloherty@users.noreply.github.com>
2020-07-08 17:03:35 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -224,6 +224,13 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidiates) in last place
 void eliminate(int min)
 {
+    for(int i=0;i<candidate_count;i++)
+    {
+        if (candidates[i].votes == min && candidates[i].eliminated == false)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     // TODO
     return;
 }
