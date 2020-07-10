e3ff2bf2cf0e6ebe88a7987a4baa71712b6f48dc
pcloherty <pcloherty@users.noreply.github.com>
2020-07-10 16:04:48 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,14 +1,26 @@
 #include "helpers.h"
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {  
+        for(int j = 0; j < width; j++)
+        { 
+             float mean = round(( image[i][j].rgbtRed +  image[i][j].rgbtGreen + image[i][j].rgbtBlue )/3.00); 
+            image[i][j].rgbtRed= mean;
+            image[i][j].rgbtBlue= mean;
+            image[i][j].rgbtGreen= mean;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
