878587663c39e18b19fb43b0e7c33e8ae3399564
pcloherty <pcloherty@users.noreply.github.com>
2020-07-17 20:11:05 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -93,7 +93,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
           {
               for(int l = -1; l<2;l++)
               {
-                  if(k >=0  && k < height && l >= 0 && l < width)
+                  if(k > -1  && k < height && l >-1 && l < width)
                   {
                     refR+=image[l][k].rgbtRed;
                     refB= refB+image[l][k].rgbtBlue;
