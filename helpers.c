b368eeccdd06c51419e489cef68c2541fc2dd42b
pcloherty <pcloherty@users.noreply.github.com>
2020-07-17 20:26:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -89,11 +89,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for ( int j = 0; j < width; j++)
         {
           float refR=0 , refG=0, refB=0, cells=0;
-          for (int k = -1; k< 2;k++)
+          for (int k = (i-1); k< (i+1);k++)
           {
-              for(int l = -1; l<2;l++)
+              for(int l =(j -1); l<(j+1);l++)
               {
-                  if(k > -1  && k < height && l >-1 && l < width)
+                  if((k > -1  && k < height) && (l > -1 && l < width))
                   {
                     refR= refR+image[l][k].rgbtRed;
                     refB= refB+image[l][k].rgbtBlue;
