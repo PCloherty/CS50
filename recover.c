3c2d41bf6c1b0f1a7bb0ec8e79b1c7b46cd04c07
pcloherty <pcloherty@users.noreply.github.com>
2020-07-22 16:04:24 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -11,40 +11,45 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover image\n");
         return 1;
     }
-    //
+    //input file empty
     if (fopen(argv[1], "r") == NULL)
     {
         printf("File failed to open.\n");
         return 1;
     }
     //open memory card
+    //input file pointer
     FILE *image = fopen(argv[1], "r");
+    //output file pointer
     FILE *recovered = NULL;
     BYTE buffer [512];
     int imgNum = 0;
     char fileName [8];
     //fread(data(size of indevidual bytes, number of bytes,file to read from))
-    while (fread(buffer,1,512,image))
+    while (fread(buffer, 1, 512, image))
     {
         //bitwise &
-        if (buffer[0]==0xff && buffer[1]== 0xd8 && buffer[2]== 0xff && (buffer[3] & 0xf0)==0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             if (imgNum == 0)
             {
                 sprintf(fileName, "%03i.jpg", imgNum);
                 recovered = fopen(fileName, "w");
-                fwrite(buffer,1,512, recovered);
+                fwrite(buffer, 1, 512, recovered);
                 imgNum++;
-            } else {
+            } 
+            else 
+            {
                 fclose(recovered);
                 sprintf(fileName, "%03i.jpg", imgNum);
                 recovered = fopen(fileName, "w");
-                 fwrite(buffer,1,512, recovered);
+                fwrite(buffer, 1, 512, recovered);
                 imgNum++;
             }
-        } else if(imgNum>0)
+        } 
+        else if (imgNum > 0)
         {
-            fwrite(buffer,1,512,recovered);
+            fwrite(buffer, 1, 512, recovered);
         }
     }
     fclose(recovered);
