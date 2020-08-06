a6c4b8047cd7a8166cff288b49389499a432ecf5
pcloherty <pcloherty@users.noreply.github.com>
2020-08-06 21:08:19 +0000

    automated commit by submit50

--- 11.sql
+++ 11.sql
@@ -1,1 +1,1 @@
-select title from movies where id in(select movie_id from stars where person_id in (select id from people where name = "Chadwick Boseman") and movie_id in(select movie_id from ratings )) order by rating limit 5
+select title from movies where id in(select movie_id from stars where person_id in (select id from people where name = "Chadwick Boseman") and movie_id in(select movie_id from ratings order by rating asc)) limit 5
