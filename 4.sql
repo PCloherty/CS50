1c33a2fb5f5329fc4f186b7233737e71cf4d34e0
pcloherty <pcloherty@users.noreply.github.com>
2020-08-06 20:52:26 +0000

    automated commit by submit50

--- 4.sql
+++ 4.sql
@@ -1,1 +1,1 @@
-Select count(title) from movies where id = (select movie_id from ratings where rating = 10.0)
+Select count(title) from movies where id in (select movie_id from ratings where rating = 10.0)
