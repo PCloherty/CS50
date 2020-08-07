eb2563b0113711415deb68c08dbd568c3e782e5d
pcloherty <pcloherty@users.noreply.github.com>
2020-08-07 13:58:10 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -1,24 +1,19 @@
-# TODO
 from cs50 import SQL
 import csv
-from sys import argv,exit
+from sys import argv, exit
+
 
 if len(argv) != 2:
     print("not enough arguments, 'python import.py characters.csv'")
     exit()
 db = SQL("sqlite:///students.db")    
 csvFile = argv[1]    
-    
 with open(f"{csvFile}", "r") as characters:
     reader = csv.DictReader(characters)
     for row in reader:
         name = row["name"].split(" ")
         if len(name) != 3:
-            name.insert(1,None)
-        #print(name)
-        db.execute('Insert into students (first, middle, last, house, birth) values(?,?,?,?,?)', name[0], name[1], name[2], row["house"], row["birth"]);
+            name.insert(1, None)
+        db.execute('Insert into students (first, middle, last, house, birth) values(?,?,?,?,?)',
+            name[0], name[1], name[2], row["house"], row["birth"])
     
-    
-
-
-#db.execute("")
